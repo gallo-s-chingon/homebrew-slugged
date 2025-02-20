d9ab426f3fee78be5767c7fa5130d7715cd98f3c
Gallo Chingon <gallochingon@Gallos-Mac-mini.local>
2025-02-20 05:57:59 -0800

    update SHA256 for v0.1b compatibilty fix

--- Formula/slugged.rb
+++ Formula/slugged.rb
@@ -2,7 +2,7 @@ class Slugged < Formula
   desc "Convert filenames to slug format: lowercase alphanumeric with single delimiters"
   homepage "https://github.com/gallo-s-chingon/slugged"
   url "https://github.com/gallo-s-chingon/slugged/archive/refs/tags/v0.1b.tar.gz"
-  sha256 "588a9f85c49b489afc5655848fbb0ec91277ab7c057c363a715ccc3c179b65a2"
+  sha256 "a5064b065124bb877fdf54132a987dd39d2ad8557c8de8814c11e235f4ea5384"
   license "MIT"
 
   def install
